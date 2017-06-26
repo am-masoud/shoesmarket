50509aa30718af4834e898a650c7c764b8dde115
amir masoud <am.mohtaji@hotmail.com>
2017-06-26 23:11:16 +0430

    Initial Commit

---
+++ shoesmarket/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shoesmarket
+//
+//  Created by amir masoud mohtaji on 6/26/17.
+//  Copyright © 2017 amir masoud mohtaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
